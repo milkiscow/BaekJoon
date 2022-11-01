2c839467c9c4585144a26c55dd7cb259a928903a
milkiscow <cheonjeonghwan04@gmail.com>
2022-11-01 12:23:33 +0900

    ing

--- if.py
+++ if.py
@@ -36,8 +36,41 @@ if x > 0:
 else:
     print(2 if y > 0 else 3)
 
-#
+#2884
+H, M = map(int, input().split())
+M -= 45
+if M < 0:
+    M += 60
+    H -= 1
+if H < 0:
+    H += 24
+print(H, M)
 
+#2525
+A, B = map(int, input().split())
+C = int(input())
+A += C // 60
+B += C % 60
+if B >= 60:
+    B -= 60
+    A += 1
+if A >= 24:
+    A -= 24
+print(A, B)
+#n=60*a+b+int(input())          숏코딩 참고.
+#print(n//60%24,n%60)
 
+#2480
+lst = input().split()
+m = []
+for i in range(3):
+    if lst[i] == lst[(i+1)%3]:
+        m.append(int(lst[i]))
+if len(m) == 3:
+    print(10000+m[0]*1000)
+elif len(m) == 1:
+    print(1000+m[0]*100)
+else:
+    print(100*int(max(lst)))
 
 
