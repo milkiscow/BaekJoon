b050a0aa87db11411d4ad522e56301df706a9ef5
milkiscow <cheonjeonghwan04@gmail.com>
2022-11-01 11:46:07 +0900

    ing

--- if.py
+++ if.py
@@ -28,10 +28,15 @@ if Y % 400 == 0 or (Y%4==0 and Y%100!=0):
 else:
     print(0)
 
+#14681
+x = int(input())
+y = int(input())
+if x > 0:
+    print(1 if y > 0 else 4)
+else:
+    print(2 if y > 0 else 3)
+
 #
-
-
-
 
 
 
