fab1c86def0862f3ac943731bf27b49e5393cc0b
milkiscow <cheonjeonghwan04@gmail.com>
2022-11-02 09:37:03 +0900

    ing

--- 1D.py
+++ 1D.py
@@ -18,14 +18,30 @@ N = int(input())
 lst = list(map(int, input().split()))
 print(min(lst), max(lst))
 
+#2562
+lst = [0]
+for i in range(9):
+    lst.append(int(input()))
+print(max(lst), lst.index(max(lst)))
 
+#5597
+lst = list(range(1,31))
+for i in range(28):
+    lst.remove(int(input()))
+print(lst[0])
+print(lst[1])
 
+#3052                   숏코딩 참고.
+b = [int(input())%42 for i in range(10)]
+print(len(set(b)))
 
-
-
-
-
-
+#1546
+N = int(input())
+lst = list(map(int, input().split()))
+M = max(lst)
+for i in range(N):
+    lst[i] = lst[i]/M*100
+print(sum(lst)/N)
 
 
 
